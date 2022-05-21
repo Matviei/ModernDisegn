ff2cbf7425489f5175ea0377e83c30335f1ed33b
stud-end <stud-end@mail.ru>
2022-05-21 17:55:31 +0300

    Добавление base

---
+++ ModernDisegn/Core/RelayCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModernDisegn.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+    }
+}
