513d98868ea2ba8ee92481494a179c61bf6ae090
Матвей Сунцов <matviei1109@yandex.ru>
2022-05-21 20:17:41 +0300

    добавление base command

--- ModernDisegn/Core/RelayCommand.cs
+++ ModernDisegn/Core/RelayCommand.cs
@@ -11,5 +11,27 @@ namespace ModernDisegn.Core
     {
         private Action<object> execute;
         private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }    
+        }
+
+        public RelayCommand(Action<object> exececute, Func<object, bool> canExecute = null)
+        {
+            this.execute = exececute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
     }
 }
